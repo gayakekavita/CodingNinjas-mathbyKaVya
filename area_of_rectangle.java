a29418176b2800b245754bb7a2cfa6b9943bb72d
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-02-20 23:55:12 +0530

    Create area_of_rectangle.java

---
+++ area_of_rectangle.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+class Solution {
+	
+	public static void main(String args[]) {
+		
+		int length;
+		int breadth;
+		Scanner sc=new Scanner(System.in);
+		length=sc.nextInt();
+		breadth=sc.nextInt();
+		System.out.println(length*breadth);
+		
+	}
+}
